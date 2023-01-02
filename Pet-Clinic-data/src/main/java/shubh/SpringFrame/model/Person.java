7bc7b7daf24e92d17e5d7b247dd5f523bc396f5e
shubham-sanvariya <shubhamsanvariyait.com>
2023-01-02 22:16:43 +0530

    adding base entity

--- Pet-Clinic-data/src/main/java/shubh/SpringFrame/model/Person.java
+++ Pet-Clinic-data/src/main/java/shubh/SpringFrame/model/Person.java
@@ -1,6 +1,6 @@
 package shubh.SpringFrame.model;
 
-public class Person {
+public class Person extends BaseEntity{
     private String firstName;
     private String lastName;
 
--- Pet-Clinic-data/src/main/java/shubh/SpringFrame/model/PetType.java
+++ Pet-Clinic-data/src/main/java/shubh/SpringFrame/model/PetType.java
@@ -1,6 +1,6 @@
 package shubh.SpringFrame.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
     private String name;
 
     public String getName() {
