dd4fc72e8e91fd262eb7af2f209be5b25155666e
shubham-sanvariya <shubhamsanvariyait.com>
2023-01-08 14:28:33 +0530

    Adding index Page and Controller

---
+++ Pet-Clinic-web/src/main/java/shubh/SpringFrame/PetClinic/Controllers/IndexController.java
@@ -0,0 +1,12 @@
+package shubh.SpringFrame.PetClinic.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping({"","/","index","index.html"})
+    public String index() {
+        return "index";
+    }
+}
