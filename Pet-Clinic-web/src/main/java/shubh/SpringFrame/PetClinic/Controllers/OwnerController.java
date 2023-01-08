d03004bb0050cc3fb93787e12ca96ddadb677a0b
shubham-sanvariya <shubhamsanvariyait.com>
2023-01-08 21:58:51 +0530

    Adding Owners Page and Controller

---
+++ Pet-Clinic-web/src/main/java/shubh/SpringFrame/PetClinic/Controllers/OwnerController.java
@@ -0,0 +1,14 @@
+package shubh.SpringFrame.PetClinic.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listOwners() {
+        return "owners/index";
+    }
+}
