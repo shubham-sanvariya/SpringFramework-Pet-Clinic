c06822cbbc62fc85c805f9644b479cb0f9c634c4
shubham-sanvariya <shubhamsanvariyait.com>
2023-01-23 23:03:57 +0530

    PetServiceMap implements PetService

--- Pet-Clinic-data/src/main/java/shubh/SpringFrame/services/map/PetServiceMap.java
+++ Pet-Clinic-data/src/main/java/shubh/SpringFrame/services/map/PetServiceMap.java
@@ -1,11 +1,11 @@
 package shubh.SpringFrame.services.map;
 
 import shubh.SpringFrame.model.Pet;
-import shubh.SpringFrame.services.CrudService;
+import shubh.SpringFrame.services.PetService;
 
 import java.util.Set;
 
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
 
     @Override
     public Set<Pet> findAll() {
