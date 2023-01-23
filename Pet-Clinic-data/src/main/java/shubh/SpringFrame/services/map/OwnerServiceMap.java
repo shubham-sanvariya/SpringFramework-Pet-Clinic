8838ef778d0bb7ca5be23332a49ce8ae40edda5e
shubham-sanvariya <shubhamsanvariyait.com>
2023-01-23 23:04:17 +0530

    OwnerServiceMap implements OwnerService

--- Pet-Clinic-data/src/main/java/shubh/SpringFrame/services/map/OwnerServiceMap.java
+++ Pet-Clinic-data/src/main/java/shubh/SpringFrame/services/map/OwnerServiceMap.java
@@ -1,11 +1,11 @@
 package shubh.SpringFrame.services.map;
 
 import shubh.SpringFrame.model.Owner;
-import shubh.SpringFrame.services.CrudService;
+import shubh.SpringFrame.services.OwnerService;
 
 import java.util.Set;
 
-public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
     @Override
     public Set<Owner> findAll() {
         return super.findAll();
@@ -29,5 +29,10 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
     @Override
     public void deleteById(Long id) {
         super.deleteById(id);
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return null;
     }
 }
