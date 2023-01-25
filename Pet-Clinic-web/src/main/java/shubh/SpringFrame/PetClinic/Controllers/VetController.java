f2ba5317f97ec3e2ff756c127e0293b67c4502d9
shubham-sanvariya <shubhamsanvariyait.com>
2023-01-25 16:34:53 +0530

    Added VetList and updated Controller

--- Pet-Clinic-web/src/main/java/shubh/SpringFrame/PetClinic/Controllers/VetController.java
+++ Pet-Clinic-web/src/main/java/shubh/SpringFrame/PetClinic/Controllers/VetController.java
@@ -1,13 +1,23 @@
 package shubh.SpringFrame.PetClinic.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import shubh.SpringFrame.PetClinic.services.VetService;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets","/vets/index","/vets/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets",vetService.findAll());
+
         return "vets/index";
     }
 }
