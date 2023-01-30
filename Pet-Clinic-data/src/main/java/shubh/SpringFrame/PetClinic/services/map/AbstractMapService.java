23ea979f47cd1af155da2315c520b64b69245325
shubham-sanvariya <shubhamsanvariyait.com>
2023-01-30 15:46:00 +0530

    Refactored map  Impl to abstract ID generation  (auto generation of ID)

--- Pet-Clinic-data/src/main/java/shubh/SpringFrame/PetClinic/services/map/AbstractMapService.java
+++ Pet-Clinic-data/src/main/java/shubh/SpringFrame/PetClinic/services/map/AbstractMapService.java
@@ -1,12 +1,11 @@
 package shubh.SpringFrame.PetClinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import shubh.SpringFrame.PetClinic.model.BaseEntity;
 
-public abstract class AbstractMapService<T, ID> {
-    protected Map<ID, T> map = new HashMap<>();
+import java.util.*;
+
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
         return new HashSet<>(map.values());
@@ -16,8 +15,15 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object) {
-        map.put(id,object);
+    T save(T object) {
+        if (object != null){
+            if (object.getId() == null){
+                object.setId(getNextId());
+            }
+            map.put(object.getId(), object);
+        } else {
+            throw new RuntimeException("Object cannot be null");
+        }
 
         return object;
     }
@@ -28,5 +34,16 @@ public abstract class AbstractMapService<T, ID> {
 
     void delete(T object) {
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+
+    private Long getNextId() {
+        Long nextId = null;
+
+        try {
+            nextId = Collections.max(map.keySet()) + 1; // if the map is not empty it will increment the value
+        } catch (NoSuchElementException e) {  // if the map is empty it will set to 1
+            nextId = 1L;
+        }
+        return nextId;
     }
 }
