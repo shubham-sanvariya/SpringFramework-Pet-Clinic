473f6d495497ba4b143c7e94dcb9de494b9c18bc
shubham-sanvariya <shubhamsanvariyait.com>
2023-02-14 16:46:57 +0530

    added Speciality and updated Vet by adding Speciality

--- Pet-Clinic-data/src/main/java/shubh/SpringFrame/PetClinic/model/Vet.java
+++ Pet-Clinic-data/src/main/java/shubh/SpringFrame/PetClinic/model/Vet.java
@@ -1,4 +1,15 @@
 package shubh.SpringFrame.PetClinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person{
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
