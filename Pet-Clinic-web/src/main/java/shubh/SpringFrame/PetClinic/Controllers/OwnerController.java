388c0841a03a12a3a7c1b76e6f883ab341442504
shubham-sanvariya <shubhamsanvariyait.com>
2023-02-15 14:28:07 +0530

    Fixed Broken Links

--- Pet-Clinic-web/src/main/java/shubh/SpringFrame/PetClinic/Controllers/OwnerController.java
+++ Pet-Clinic-web/src/main/java/shubh/SpringFrame/PetClinic/Controllers/OwnerController.java
@@ -22,4 +22,9 @@ public class OwnerController {
 
         return "owners/index";
     }
+
+    @RequestMapping({"/find"})
+    public String findOwners() {
+        return "notImplemented.html";
+    }
 }
