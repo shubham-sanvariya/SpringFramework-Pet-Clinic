0e078ab143a23c1bfe7194bb66f1687a26c212d0
shubham-sanvariya <shubhamsanvariyait.com>
2023-02-27 22:29:28 +0530

    adding @MappedSuperclass and setting GenerationType

--- Pet-Clinic-data/src/main/java/shubh/SpringFrame/PetClinic/model/BaseEntity.java
+++ Pet-Clinic-data/src/main/java/shubh/SpringFrame/PetClinic/model/BaseEntity.java
@@ -1,8 +1,17 @@
 package shubh.SpringFrame.PetClinic.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
