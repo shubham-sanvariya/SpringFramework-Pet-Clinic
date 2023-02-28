8b7d2081a7ca7bedad3138d1683c1654883c08cf
shubham-sanvariya <shubhamsanvariyait.com>
2023-02-28 13:42:49 +0530

    updated vet to use JPA Entity

--- Pet-Clinic-data/src/main/java/shubh/SpringFrame/PetClinic/model/Speciality.java
+++ Pet-Clinic-data/src/main/java/shubh/SpringFrame/PetClinic/model/Speciality.java
@@ -1,6 +1,14 @@
 package shubh.SpringFrame.PetClinic.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity{
+
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
--- Pet-Clinic-data/src/main/java/shubh/SpringFrame/PetClinic/model/Vet.java
+++ Pet-Clinic-data/src/main/java/shubh/SpringFrame/PetClinic/model/Vet.java
@@ -1,9 +1,18 @@
 package shubh.SpringFrame.PetClinic.model;
 
+import jakarta.persistence.*;
+
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person{
+
+    @ManyToMany(fetch = FetchType.EAGER) // it tells the jpa to load at once
+    @JoinTable(name = "vet_specialties" ,
+                joinColumns = @JoinColumn(name = "vet_id"),
+                inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
