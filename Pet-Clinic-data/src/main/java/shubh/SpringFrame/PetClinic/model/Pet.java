9333673d8dc4a6aaae0b5001ee922649fdda962b
shubham-sanvariya <shubhamsanvariyait.com>
2023-02-28 15:50:08 +0530

    added visit entity & set up relation with Pet

--- Pet-Clinic-data/src/main/java/shubh/SpringFrame/PetClinic/model/Pet.java
+++ Pet-Clinic-data/src/main/java/shubh/SpringFrame/PetClinic/model/Pet.java
@@ -3,6 +3,8 @@ package shubh.SpringFrame.PetClinic.model;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -17,6 +19,9 @@ public class Pet extends BaseEntity{
     private Owner owner;
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
@@ -48,5 +53,13 @@ public class Pet extends BaseEntity{
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
