77607c37b5746f4be8f770ce126f80e0e5a1a880
shubham-sanvariya <shubhamsanvariyait.com>
2023-02-28 12:35:01 +0530

    adding Columns & setting up entities and relational mapping

--- Pet-Clinic-data/src/main/java/shubh/SpringFrame/PetClinic/model/Person.java
+++ Pet-Clinic-data/src/main/java/shubh/SpringFrame/PetClinic/model/Person.java
@@ -1,7 +1,13 @@
 package shubh.SpringFrame.PetClinic.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person extends BaseEntity{
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
 
     public String getFirstName() {
