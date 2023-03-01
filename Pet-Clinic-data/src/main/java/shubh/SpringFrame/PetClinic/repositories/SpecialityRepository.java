1c02ef8cfccfd6fe185a64d37dfd64eaabeb0b6f
shubham-sanvariya <shubhamsanvariyait.com>
2023-03-01 20:36:00 +0530

    changed small s to S in speciality

--- Pet-Clinic-data/src/main/java/shubh/SpringFrame/PetClinic/repositories/SpecialityRepository.java
+++ Pet-Clinic-data/src/main/java/shubh/SpringFrame/PetClinic/repositories/SpecialityRepository.java
@@ -3,5 +3,5 @@ package shubh.SpringFrame.PetClinic.repositories;
 import org.springframework.data.repository.CrudRepository;
 import shubh.SpringFrame.PetClinic.model.Speciality;
 
-public interface specialityRepository extends CrudRepository<Speciality, Long> {
+public interface SpecialityRepository extends CrudRepository<Speciality, Long> {
 }
