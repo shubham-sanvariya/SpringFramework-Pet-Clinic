6b844cad2968e5343a09010d3254bb6f54033517
shubham-sanvariya <shubhamsanvariyait.com>
2023-03-12 18:28:52 +0530

    starting with jUnit 5

--- Pet-Clinic-web/src/test/java/shubh/SpringFrame/PetClinic/PetClinicApplicationTests.java
+++ Pet-Clinic-web/src/test/java/shubh/SpringFrame/PetClinic/PetClinicApplicationTests.java
@@ -1,8 +1,11 @@
 package shubh.SpringFrame.PetClinic;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class PetClinicApplicationTests {
 
