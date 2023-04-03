daef4ad02e86051ea1b77afe36909e06911460fc
shubham-sanvariya <shubhamsanvariyait.com>
2023-04-03 14:07:05 +0530

    adding GetMapping for ownerId to show Owner

--- Pet-Clinic-web/src/main/java/shubh/SpringFrame/PetClinic/Controllers/OwnerController.java
+++ Pet-Clinic-web/src/main/java/shubh/SpringFrame/PetClinic/Controllers/OwnerController.java
@@ -2,7 +2,10 @@ package shubh.SpringFrame.PetClinic.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 import shubh.SpringFrame.PetClinic.services.OwnerService;
 
 @RequestMapping("/owners")
@@ -26,5 +29,12 @@ public class OwnerController {
     @RequestMapping({"/find"})
     public String findOwners() {
         return "notImplemented.html";
+    }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(this.ownerService.findById(ownerId));
+        return mav;
     }
 }
