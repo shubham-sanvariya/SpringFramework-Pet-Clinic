e3cf3e52c468b98715c48b3c54d6461786af459b
shubham-sanvariya <shubhamsanvariyait.com>
2023-04-03 14:07:18 +0530

    testing GetMapping for ownerId to show Owner

--- Pet-Clinic-web/src/test/java/shubh/SpringFrame/PetClinic/Controllers/OwnerControllerTest.java
+++ Pet-Clinic-web/src/test/java/shubh/SpringFrame/PetClinic/Controllers/OwnerControllerTest.java
@@ -15,7 +15,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -71,5 +72,15 @@ class OwnerControllerTest {
                 .andExpect(view().name("notImplemented.html"));
 
         verifyNoInteractions(ownerService);
+    }
+
+    @Test
+    void displayOwner() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1l).build());
+
+        mockMvc.perform(get("/owners/123"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownerDetails"))
+                .andExpect(model().attribute("owner", hasProperty("id", is(1l))));
     }
 }
