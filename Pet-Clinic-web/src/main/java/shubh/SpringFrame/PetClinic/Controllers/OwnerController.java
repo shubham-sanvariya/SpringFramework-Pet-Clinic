3baca08308d073d1654fd419178f8ac006ae9e9e
shubham-sanvariya <shubhamsanvariyait.com>
2023-04-04 15:17:13 +0530

    adding InitBinder in Controller

--- Pet-Clinic-web/src/main/java/shubh/SpringFrame/PetClinic/Controllers/OwnerController.java
+++ Pet-Clinic-web/src/main/java/shubh/SpringFrame/PetClinic/Controllers/OwnerController.java
@@ -2,7 +2,9 @@ package shubh.SpringFrame.PetClinic.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -16,6 +18,11 @@ public class OwnerController {
 
     public OwnerController(OwnerService ownerService) {
         this.ownerService = ownerService;
+    }
+
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder){
+        dataBinder.setDisallowedFields("id");
     }
 
     @RequestMapping({"", "/", "/index", "/index.html"})
