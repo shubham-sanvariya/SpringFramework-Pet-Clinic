d407fea7dc0188c4074424f0b075168b505f45a7
shubham-sanvariya <shubhamsanvariyait.com>
2023-04-04 15:16:13 +0530

    adding findByLastName of Owner through List

--- Pet-Clinic-data/src/main/java/shubh/SpringFrame/PetClinic/repositories/OwnerRepository.java
+++ Pet-Clinic-data/src/main/java/shubh/SpringFrame/PetClinic/repositories/OwnerRepository.java
@@ -3,6 +3,10 @@ package shubh.SpringFrame.PetClinic.repositories;
 import org.springframework.data.repository.CrudRepository;
 import shubh.SpringFrame.PetClinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
     Owner findByLastName(String lastName);
+
+    List<Owner> findByLastNameLike(String lastName);
 }
--- Pet-Clinic-data/src/main/java/shubh/SpringFrame/PetClinic/services/OwnerService.java
+++ Pet-Clinic-data/src/main/java/shubh/SpringFrame/PetClinic/services/OwnerService.java
@@ -2,6 +2,10 @@ package shubh.SpringFrame.PetClinic.services;
 
 import shubh.SpringFrame.PetClinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner,Long>{
     Owner findByLastName(String lastName);
+
+    List<Owner> findByLastNameLike(String lastName);
 }
