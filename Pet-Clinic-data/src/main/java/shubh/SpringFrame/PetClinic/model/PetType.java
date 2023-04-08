fcfbf66d50ef725df8f6fc82efcae23380fa4157
shubham-sanvariya <shubhamsanvariyait.com>
2023-04-08 13:35:49 +0530

    adding builder pattern to PetType

--- Pet-Clinic-data/src/main/java/shubh/SpringFrame/PetClinic/model/PetType.java
+++ Pet-Clinic-data/src/main/java/shubh/SpringFrame/PetClinic/model/PetType.java
@@ -9,10 +9,15 @@ import lombok.*;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 @Entity
 @Table(name = "types")
 public class PetType extends BaseEntity{
+
+    @Builder
+    public PetType(Long id, String  name){
+        super(id);
+        this.name = name;
+    }
 
     @Column(name = "name")
     private String name;
