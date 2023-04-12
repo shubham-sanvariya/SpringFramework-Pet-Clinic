8377cb8c00e280dd293bb53c5ab285fcbe75dde5
shubham-sanvariya <shubhamsanvariyait.com>
2023-04-12 15:57:48 +0530

    added json endpoint

--- Pet-Clinic-web/src/main/java/shubh/SpringFrame/PetClinic/Controllers/VetController.java
+++ Pet-Clinic-web/src/main/java/shubh/SpringFrame/PetClinic/Controllers/VetController.java
@@ -3,7 +3,11 @@ package shubh.SpringFrame.PetClinic.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import shubh.SpringFrame.PetClinic.model.Vet;
 import shubh.SpringFrame.PetClinic.services.VetService;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -19,5 +23,11 @@ public class VetController {
         model.addAttribute("vets",vetService.findAll());
 
         return "vets/index";
+    }
+
+
+    @RequestMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson () {
+        return vetService.findAll();
     }
 }
