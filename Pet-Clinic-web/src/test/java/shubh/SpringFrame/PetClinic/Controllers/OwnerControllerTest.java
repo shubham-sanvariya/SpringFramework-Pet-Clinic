06a00401234d12a8c811fd04ce9198bb06f1b1f3
shubham-sanvariya <shubhamsanvariyait.com>
2023-04-12 13:09:00 +0530

    fixed test to find all owners

--- Pet-Clinic-web/src/test/java/shubh/SpringFrame/PetClinic/Controllers/OwnerControllerTest.java
+++ Pet-Clinic-web/src/test/java/shubh/SpringFrame/PetClinic/Controllers/OwnerControllerTest.java
@@ -80,18 +80,18 @@ class OwnerControllerTest {
                 .andExpect(view().name("redirect:/owners/1"));
     }
 
-//    @Test
-//    void processFindFromReturnMany() throws Exception {
-//        when(ownerService.findAllByLastNameLike(anyString()))
-//                .thenReturn(Arrays.asList(Owner.builder().id(1l).build()))
-//                .thenReturn(Arrays.asList(Owner.builder().id(2l).build()));
-//
-//        mockMvc.perform(get("/owners").param("lastName", ""))
-//                .andExpect(status().isOk())
-//                .andExpect(view().name("/owners/ownersList"))
-//                .andExpect(model().attribute("selections", hasSize(2))));
-//
-//    }
+    @Test
+    void processFindFromReturnMany() throws Exception {
+        when(ownerService.findAllByLastNameLike(anyString()))
+                .thenReturn(Arrays.asList(Owner.builder().id(1l).build(),
+                        Owner.builder().id(2l).build()));
+
+        mockMvc.perform(get("/owners").param("lastName", ""))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownersList"))
+                .andExpect(model().attribute("selections", hasSize(2)));
+
+    }
 
     @Test
     void displayOwner() throws Exception {
